a940d1d94fffc1a9b9831ce10a2ccb6ba78d4500
johansballestar <johansballestar@gmail.com>
2022-03-12 19:51:21 -0600

    tailwind

--- pages/raffle.js
+++ pages/raffle.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { ethers } from "ethers"
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import Web3Modal from "web3modal"
+import navbar from '../components/navbar'
+import { useRouter } from 'next/router'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+import {addRaffle, getAllRaffles} from '../utils/raffles';
 
 const raffle = () => {
   return (
